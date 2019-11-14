9a387bc7a70611aff572a2c3249b584a11ecc88a
xqc950217 <1006866091@qq.com>
2019-11-14 19:50:05 +0800

    黑马头条PC-主页模块-左侧导航菜单

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,26 +1,63 @@
 <template>
   <el-container>
-  <el-aside width="200px">Aside</el-aside>
-  <el-container>
-    <el-header>Header</el-header>
-    <el-main>
+    <!-- 左侧内容 -->
+    <el-aside class='left' style='width:220px'>
+      <div class='menu-title' style="background-color:#2e2f32">
+        <img src="../../assets/img/logo_admin.png" alt="">
+      </div>
+      <!-- 左侧菜单 -->
+      <el-menu style="width:221px" background-color="#353b4e" text-color="#adafb5" active-text-color="#ffd04b">
+        <!-- 一级菜单 -->
+        <el-menu-item>首页</el-menu-item>
+        <!-- 二级菜单 -->
+        <el-submenu>
+          <!-- 具名插槽 -->
+          <template slot="title">内容管理</template>
+          <el-menu-item>内容发布</el-menu-item>
+          <el-menu-item>内容列表</el-menu-item>
+          <el-menu-item>评论列表</el-menu-item>
+          <el-menu-item>素材管理</el-menu-item>
+        </el-submenu>
+        <!-- 二级菜单 -->
+        <el-submenu>
+          <template slot="title">粉丝管理</template>
+          <el-menu-item>内容发布</el-menu-item>
+          <el-menu-item>内容列表</el-menu-item>
+          <el-menu-item>评论列表</el-menu-item>
+          <el-menu-item>素材管理</el-menu-item>
+        </el-submenu>
+        <el-menu-item>账户信息</el-menu-item>
+      </el-menu>
+    </el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>
         <!-- 二级路由容器 -->
         <router-view></router-view>
-    </el-main>
-
+      </el-main>
+    </el-container>
   </el-container>
-</el-container>
 </template>
 
 <script>
 export default {
   name: 'home',
-  components: {
-
-  }
+  components: {}
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.left {
+  width:220px;
+  height: 100vh;
+  background-color:#353b4e;
+  overflow:hidden;
+  .menu-title {
+  padding: 15px 0;
+  text-align: center;
+  img {
+    height:30px;
+  }
+ }
+}
 </style>
