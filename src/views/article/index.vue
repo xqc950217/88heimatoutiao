9d47f1c2520c028312d2e85ff2f30a413dfadf55
xqc950217 <1006866091@qq.com>
2019-11-15 21:27:26 +0800

    文章列表布局

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,13 +1,113 @@
 <template>
-  <div class="article">内容列表</div>
+  <div class="article">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>全部图文</span>
+      </div>
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="文章状态">
+          <el-radio-group v-model="filterFrom">
+            <el-radio label="全部"></el-radio>
+            <el-radio label="草稿"></el-radio>
+            <el-radio label="待审核"></el-radio>
+            <el-radio label="审核通过"></el-radio>
+            <el-radio label="审核失败"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+
+        <el-form-item label="频道列表">
+          <el-select placeholder="请选择" v-model="filterFrom.channel_id">
+            <el-option label="开发者资讯" value="shanghai"></el-option>
+            <el-option label="iOS" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="时间选择">
+          <el-date-picker
+            v-model="rangeDate"
+            type="daterange"
+            align="right"
+            unlink-panels
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+            :picker-options="pickerOptions"
+          ></el-date-picker>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">查询</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <!-- 文章列表 -->
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>共找到59806条符合条件的内容</span>
+      </div>
+       <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="封面"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="标题"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="状态">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="发布日期">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="操作">
+      </el-table-column>
+    </el-table>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-
+  // 建议给每个组建起一个名字 好找
+  name: 'article',
+  data () {
+    return {
+      //    过滤表单数据
+      filterFrom: {
+        status: '',
+        channel_id: '',
+        begin_pubdate: '',
+        end_pubdate: ''
+      },
+      rangeDate: '',
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
