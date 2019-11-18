cc76f45793c53617abf25a1b5d2cf2dd90abf62c
xqc950217 <1006866091@qq.com>
2019-11-18 21:41:37 +0800

    评论管理-修改评论状态

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -8,9 +8,13 @@
       <el-table-column prop="title" label="标题" width="180"></el-table-column>
       <el-table-column prop="total_comment_count" label="总评论数" width="180"></el-table-column>
       <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
-      <el-table-column prop="comment_status" label="评论状态">
-        <template>
-          <el-switch v-model="value" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+      <el-table-column  label="评论状态">
+        <template slot-scope="scope">
+          <el-switch v-model="scope.row.comment_status"
+          active-color="#13ce66"
+           inactive-color="#ff4949"
+          @change="onStatusChange(scope.row)"
+           ></el-switch>
         </template>
       </el-table-column>
       <el-table-column  label="操作">
@@ -27,7 +31,7 @@
 <script>
 export default {
   name: 'CommentIndex',
-  comments: {},
+  components: {},
   props: {},
   data () {
     return {
@@ -54,6 +58,27 @@ export default {
         .catch(err => {
           console.log(err, '获取失败')
         })
+    },
+    onStatusChange (article) {
+      this.$axios({
+        method: 'PUT',
+        url: '/comments/status',
+        params: {
+          article_id: article.id.toString()
+        },
+        data: {
+          allow_comment: article.comment_status
+        }
+      }).then(res => {
+        console.log(res)
+        this.$message({
+          type: 'success',
+          message: `${article.comment_status ? '启用' : '关闭'}成功`
+        })
+      }).catch(err => {
+        console.log(err)
+        this.$message.error('操作失败')
+      })
     }
   }
 }
