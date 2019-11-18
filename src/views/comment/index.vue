d9bf25dc3bf86376e7d186f946c72b830356c5db
xqc950217 <1006866091@qq.com>
2019-11-18 22:02:07 +0800

    评论管理-分页完成

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,30 +1,37 @@
 <template>
-  <div>
-      <el-card class="box-card">
+  <div class="article">
+    <el-card class="box-card">
       <div slot="header" class="clearfix">
         <span>评论管理</span>
       </div>
-    <el-table :data="articles" style="width: 100%">
-      <el-table-column prop="title" label="标题" width="180"></el-table-column>
-      <el-table-column prop="total_comment_count" label="总评论数" width="180"></el-table-column>
-      <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
-      <el-table-column  label="评论状态">
-        <template slot-scope="scope">
-          <el-switch v-model="scope.row.comment_status"
-          active-color="#13ce66"
-           inactive-color="#ff4949"
-          @change="onStatusChange(scope.row)"
-           ></el-switch>
-        </template>
-      </el-table-column>
-      <el-table-column  label="操作">
-        <template>
-          <el-button type="primary">修改</el-button>
-          <el-button type="primary">关闭评论</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+      <el-table :data="articles" style="width: 100%">
+        <el-table-column prop="title" label="标题" width="180"></el-table-column>
+        <el-table-column prop="total_comment_count" label="总评论数" width="180"></el-table-column>
+        <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+        <el-table-column label="评论状态">
+          <template slot-scope="scope">
+            <el-switch
+              v-model="scope.row.comment_status"
+              active-color="#13ce66"
+              inactive-color="#ff4949"
+              @change="onStatusChange(scope.row)"
+            ></el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template>
+            <el-button type="primary">修改</el-button>
+            <el-button type="primary">关闭评论</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </el-card>
+    <el-pagination
+    background
+     layout="prev, pager, next"
+     :total="totalCount"
+     @current-change="onPageChange"
+     ></el-pagination>
   </div>
 </template>
 
@@ -35,29 +42,37 @@ export default {
   props: {},
   data () {
     return {
-      articles: []
+      articles: [],
+      totalCount: 0
     }
   },
   computed: {},
   watch: {},
   created () {
-    this.loadArticles()
+    this.loadArticles(1)
   },
   methods: {
-    loadArticles () {
+    loadArticles (page = 1) {
       this.$axios({
         method: 'GET',
         url: '/articles',
         params: {
-          response_type: 'comment'
+          response_type: 'comment',
+          page,
+          per_page: 10
+
         }
       })
         .then(res => {
           this.articles = res.data.data.results
+          this.totalCount = res.data.data.total_count
         })
         .catch(err => {
           console.log(err, '获取失败')
         })
+    },
+    onPageChange (page) {
+      this.loadArticles(page)
     },
     onStatusChange (article) {
       this.$axios({
@@ -69,20 +84,27 @@ export default {
         data: {
           allow_comment: article.comment_status
         }
-      }).then(res => {
-        console.log(res)
-        this.$message({
-          type: 'success',
-          message: `${article.comment_status ? '启用' : '关闭'}成功`
-        })
-      }).catch(err => {
-        console.log(err)
-        this.$message.error('操作失败')
       })
+        .then(res => {
+          console.log(res)
+          this.$message({
+            type: 'success',
+            message: `${article.comment_status ? '启用' : '关闭'}成功`
+          })
+        })
+        .catch(err => {
+          console.log(err)
+          this.$message.error('操作失败')
+        })
     }
   }
 }
 </script>
 
-<style>
+<style scoped lang="less">
+.article {
+  .box-card {
+    margin-bottom: 20px;
+  }
+}
 </style>
