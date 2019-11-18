1ba2c33d57fbc303bca5831b14ee8f2b925a5177
xqc950217 <1006866091@qq.com>
2019-11-18 21:00:51 +0800

    评论管理-展示评论列表正确的

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,37 +1,26 @@
 <template>
   <div>
-
-      <el-table
-      :data="tableData"
-      style="width: 100%">
-      <el-table-column
-        prop="date"
-        label="标题"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="总评论数"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="粉丝评论数">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="评论状态">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="操作">
+      <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>评论管理</span>
+      </div>
+    <el-table :data="articles" style="width: 100%">
+      <el-table-column prop="title" label="标题" width="180"></el-table-column>
+      <el-table-column prop="total_comment_count" label="总评论数" width="180"></el-table-column>
+      <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+      <el-table-column prop="comment_status" label="评论状态">
         <template>
-            <el-button type="primary">修改</el-button>
-            <el-button type="primary">关闭评论</el-button>
+          <el-switch v-model="value" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column  label="操作">
+        <template>
+          <el-button type="primary">修改</el-button>
+          <el-button type="primary">关闭评论</el-button>
         </template>
       </el-table-column>
     </el-table>
-
+    </el-card>
   </div>
 </template>
 
@@ -46,9 +35,7 @@ export default {
     }
   },
   computed: {},
-  watch: {
-
-  },
+  watch: {},
   created () {
     this.loadArticles()
   },
@@ -60,11 +47,13 @@ export default {
         params: {
           response_type: 'comment'
         }
-      }).then(res => {
-        this.articles = res.data.data.results
-      }).catch(err => {
-        console.log(err, '获取失败')
       })
+        .then(res => {
+          this.articles = res.data.data.results
+        })
+        .catch(err => {
+          console.log(err, '获取失败')
+        })
     }
   }
 }
