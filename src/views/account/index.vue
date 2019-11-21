43bfa4bd1c1f2d03ad55a2dbef1e26982e685f9a
xqc950217 <1006866091@qq.com>
2019-11-21 20:33:16 +0800

    展示账户信息

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -6,18 +6,18 @@
           class="avatar-uploader"
           action="https://jsonplaceholder.typicode.com/posts/"
           :show-file-list="false">
-          <img width="100" src="http://toutiao.meiduo.site/FuLZH0vzKbdXi-On88NArXegOjXk" class="avatar">
+          <img width="100" src="user.photo" class="avatar">
           <!-- <i class="el-icon-plus avatar-uploader-icon"></i> -->
         </el-upload>
       </el-form-item>
       <el-form-item label="用户昵称">
-        <el-input v-model="form.name"></el-input>
+        <el-input v-model="user.name"></el-input>
       </el-form-item>
       <el-form-item label="个人介绍">
-        <el-input type="textarea" v-model="form.name"></el-input>
+        <el-input type="textarea" v-model="user.intro"></el-input>
       </el-form-item>
       <el-form-item label="用户邮箱">
-        <el-input v-model="form.name"></el-input>
+        <el-input v-model="user.email"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit">保存修改</el-button>
@@ -30,26 +30,38 @@
 export default {
   data () {
     return {
-      form: {
+      user: {
+        email: '',
+        intro: '',
+        mobile: '',
         name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+        photo: false
+
       }
     }
   },
+  created () {
+    this.loadUserProfile()
+  },
   methods: {
     onSubmit () {
-      console.log('submit!')
+      console.log('保存!')
+    },
+    loadUserProfile () {
+      this.$axios({
+        method: 'GET',
+        url: '/user/profile'
+      }).then(res => {
+        this.user = res.data.data
+      }).catch(err => {
+        console.log(err)
+        this.$message.error('获取失败')
+      })
     }
   }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
 
 </style>
